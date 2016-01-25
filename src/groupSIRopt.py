e7b4930f16730292b172f0b853ca46533b3366cb
路云飞 <luyunfeicx@163.com>
2016-01-26 01:34:10 +0800

    fix single

--- src/groupSIRopt.py
+++ src/groupSIRopt.py
@@ -4,7 +4,7 @@ import disGroupData
 from lmfit import report_fit
 
 
-fw=open('../../multi_res/params.csv','w')
+fw=open('../../single_res/params.csv','w')
 peaks = {}
 for i in range(135672):
     rawdata=loadGroupData.load_data(i)
@@ -14,7 +14,7 @@ for i in range(135672):
     report_fit(result.params)
     fw.write(str(result.params))
     fw.write('\n')
-    path='../../multi_res/'
+    path='../../single_res/'
     #disGroupData.display_bi(rawdata,result,result_test,path+str(i)+'.png')
     p = disGroupData.display(rawdata,result,path+str(i)+'.png')
     if peaks.has_key(p):
--- src/optGroupData.py
+++ src/optGroupData.py
@@ -32,7 +32,8 @@ def params2fcnval(params,nsteps):
     lambdaval=params['lambdaval'].value
     deltaval=params['delta'].value
     res=SIRd.SIRdecayImpulse(gamma,alpha,beta,G,n,nsteps,lambdaval,deltaval)
-    res = SIRd.SI(beta, G, n, nsteps)
+    #SI SIR and SIRd
+    #res = SIRd.SI(beta, G, n, nsteps)
     return res
 
 def fcn2minSingleImpulse(params,x,data):
@@ -110,7 +111,7 @@ def optimize(rawdata):
     x=np.array([rawdata[0],rawdata[2]])
     data=np.array([rawdata[1],rawdata[3]])
     seq=rawdata[1]
-    '''
+    
     #Single Impulse
     [lambdaval,deltaval]=singleImpulse(rawdata[1],10)
     if deltaval > -1:
@@ -122,7 +123,7 @@ def optimize(rawdata):
         if i > -1:
             i = rawdata[0][i]
     #Multi Impulse End
-    
+    '''
     Gmin=int(max(data[0]))
     ninit=data[0][0]
     params=Parameters()
@@ -131,7 +132,7 @@ def optimize(rawdata):
     params.add('gamma',value=0.05,min=0.0,max=1.0)
     params.add('G',value=2*Gmin,min=Gmin,max=2000)
     params.add('n',value=ninit,vary=False)
-    '''
+    
     #Single Impulse
     params.add('lambdaval',value=lambdaval,min=0,max=lambdaval+1)
     params.add('delta',value=deltaval,vary=False)
@@ -151,5 +152,5 @@ def optimize(rawdata):
     '''
     #Multi Impulse
     result=minimize(fcn2minMultiImpulse,params,args=(x,data,peaks))
-    
+    '''
     return result
