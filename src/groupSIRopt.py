7bca1eccf9b37e456c7abee2f02c5b9d078d33b1
路云飞 <luyunfeicx@163.com>
2016-01-26 01:44:54 +0800

    switch to SIRd

--- src/groupSIRopt.py
+++ src/groupSIRopt.py
@@ -4,7 +4,7 @@ import disGroupData
 from lmfit import report_fit
 
 
-fw=open('../../multi_res/params.csv','w')
+fw=open('../../sird_res/params.csv','w')
 peaks = {}
 for i in range(135672):
     rawdata=loadGroupData.load_data(i)
@@ -14,13 +14,13 @@ for i in range(135672):
     report_fit(result.params)
     fw.write(str(result.params))
     fw.write('\n')
-    path='../../multi_res/'
+    path='../../sird_res/'
     #disGroupData.display_bi(rawdata,result,result_test,path+str(i)+'.png')
     p = disGroupData.display(rawdata,result,path+str(i)+'.png')
     if peaks.has_key(p):
         peaks[p] += 1
     else:
         peaks[p] = 1
-    print 'No.' + str(i) + ' items finished.'
+    print 'No.' + str(i) + ' items finished.(SIRd)'
 fw.close()
 print peaks
