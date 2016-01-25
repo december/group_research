77d64ed7c2a44d2da027c2d43324538eb7feb117
路云飞 <luyunfeicx@163.com>
2016-01-26 01:23:51 +0800

    switch to multi

--- src/groupSIRopt.py
+++ src/groupSIRopt.py
@@ -4,7 +4,7 @@ import disGroupData
 from lmfit import report_fit
 
 
-fw=open('../../single_res/params.csv','w')
+fw=open('../../multi_res/params.csv','w')
 peaks = {}
 for i in range(135672):
     rawdata=loadGroupData.load_data(i)
@@ -14,12 +14,13 @@ for i in range(135672):
     report_fit(result.params)
     fw.write(str(result.params))
     fw.write('\n')
-    path='../../single_res/'
+    path='../../multi_res/'
     #disGroupData.display_bi(rawdata,result,result_test,path+str(i)+'.png')
     p = disGroupData.display(rawdata,result,path+str(i)+'.png')
     if peaks.has_key(p):
         peaks[p] += 1
     else:
         peaks[p] = 1
+    print 'No.' + str(i) + ' items finished.'
 fw.close()
 print peaks
